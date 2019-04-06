183318190a3314196e590bca54dfe25c276ed8b9
dnagarkar <devashri@gmail.com>
2019-04-05 23:34:46 -0500

    edited bank

--- code_bank.py
+++ code_bank.py
@@ -1,16 +1,16 @@
-#generate a fake data batch using the generator
 fake_data_batch = G.forward(noise)
 
 
-#Calculate the discriminator output for real and fake data
 real_scores = D.forward(real_images)
+
+
 fake_scores = D.forward(fake_data_batch)
 
-#Calculate the discriminator output for fake data
+
 fake_scores = D.forward(fake_images)
 
-#generate a fake data batch using the generator
+
 fake_images = G.forward(noise)
 
-#compute generator loss
+
 g_error = generator_loss(fake_scores)
